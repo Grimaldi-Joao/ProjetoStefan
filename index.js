651f4350b440289adb5cf0995bb1bae9d429772c
Joao Victor Da Fonte Grimaldi <jvgrimaldi@stefanini.com>
2024-03-18 20:32:13 -0300

    Ajeitado

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ var check_Ma = document.getElementById("L_Minuscula");
 var check_Num = document.getElementById("Numeros");
 var check_Simb = document.getElementById("Simbolos");
 
-const r_barra = document.querySelector(".barra input");
+const r_barra = document.querySelector(".barra");
 const input_Senha = document.querySelector(".input_box input");
 const confs = document.querySelectorAll(".conf input");
 var gerar_Senha = document.querySelector(".gerador");
 const t_senha = document.querySelector(".tamanho input");
+const icone_Copiar = document.querySelector(".input_box span");
+var largura_DaBarra = 0;
 
 
 
@@ -98,33 +100,60 @@ const test_Char = {
     Numeros : /[0-9]/,
     Simbolos: /[!@#$%¨&*(){}?:;/|'<>.,]/
 }
-const Forca = () =>{
-    r_barra.id = r_barra.value <= 24 ? "Fraca" : r_barra.value <= 49 ? "Media" : "Forte";
+
+const Forca = (largura_DaBarra) =>{
+    r_barra.id = largura_DaBarra <= 96 ? "Fraco" : largura_DaBarra <= 196 ? "Media" : "Forte";
+    switch (r_barra.id) {
+        case "Fraco":
+            r_barra.style.background = "red"
+            break;
+        
+        case "Media":
+            r_barra.style.background = "yellow"
+            break;
+        
+        case "Forte":
+            r_barra.style.background = "green"
+        break;
+
+        default:
+            break;
+    }
 }
 
+
 const att_Forca = () =>{
-    var count = 0;
-    
+    var cont = 0;
     for(const char of Object.values(test_Char)){
         if(char.test(input_Senha.value)){
-            count += 10;
+            cont += 40
         }
     }
     const valores=[8,12,16,20,26]
     for(const tam of valores){
         if (input_Senha.value.length >= tam) {
-            count += 10;
+            cont += 40
         }
     }
     if (check_dupli.checked === true) {
-        count += 10;
+        cont += 40
     }
-    r_barra.setAttribute("value", count);
+    largura_DaBarra = cont;
+    r_barra.style.width = cont +"px";
     Forca();
     return r_barra;
 }
 
 
+const copiar = () =>{
+    navigator.clipboard.writeText(input_Senha.value);
+    icone_Copiar.innerText = "check";
+    setTimeout(() =>{
+        icone_Copiar.innerText = "copy_all";
+    })
+}
+
+icone_Copiar.addEventListener("click", () => {copiar()})
 gerar_Senha.addEventListener("click", () => {gerarsenha()});
 gerar_Senha.addEventListener("click", () => {att_Forca()});
 
